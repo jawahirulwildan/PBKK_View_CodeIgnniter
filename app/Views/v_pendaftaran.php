9f58044e6401f7f95edc4993f72d113a97904d46
jawahirulwildan <jawahirulwildan@gmail.com>
2023-12-03 16:52:15 +0700

    Update v_pendaftaran.php

--- app/Views/v_pendaftaran.php
+++ app/Views/v_pendaftaran.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PPDB Online SMA Informatika 2023</title>
+    <title>PPDB Online SMAN 1 Pakong 2023</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="shortcut icon" href="https://upload.wikimedia.org/wikipedia/commons/9/9c/Logo_of_Ministry_of_Education_and_Culture_of_Republic_of_Indonesia.svg" type="image/x-icon">
     <script src="https://kit.fontawesome.com/82d0e7f3eb.js" crossorigin="anonymous"></script>
